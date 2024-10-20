a640c1f7fca244a332fb506eb80932be4f428d64
Randall Keur <rkeur7@gmail.com>
2024-10-20 13:29:01 -0400

    reformat

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+
 
 def register(request):
     if request.method != "POST":

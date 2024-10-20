f10145448f0b9abe0e49489f250441b506a48326
RandallsBagels <59258405+RandallKeur@users.noreply.github.com>
2024-10-20 11:55:35 -0400

    part 12 changes (#3)

---
+++ blogs/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.contrib import admin
+from blogs import views
+
+app_name = "blogs"
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("blogs", views.blogs, name="blogs"),
+    path("blogs/<int:blog_id>/", views.blog, name="blog"),
+    path("posts/<int:post_id>/", views.post, name="post"),
+    path("", views.index, name="index")
+]

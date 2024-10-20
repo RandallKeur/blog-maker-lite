09f96725545fe86fb3e69db9db76d91eda980dd7
Randall Keur <rkeur7@gmail.com>
2024-10-20 13:28:43 -0400

    part 14 changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import urls as auth_urls
 from django.urls import path, include
 
+from accounts import views
+
 app_name = 'accounts'
 urlpatterns = [
-    path("", include(auth_urls))
+    path("", include(auth_urls)),
+    path("register/", views.register, name="register"),
 ]

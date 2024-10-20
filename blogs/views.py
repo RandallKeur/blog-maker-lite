ff21529276fde257f0468be68d7ede401700dea7
Randall Keur <rkeur7@gmail.com>
2024-10-20 15:28:51 -0400

    reformat

--- blogs/views.py
+++ blogs/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
-from .models import Blog, BlogPost
 from .forms import BlogForm
+from .models import Blog, BlogPost
 
 
 def index(request):
